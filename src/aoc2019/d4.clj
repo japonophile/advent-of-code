fecefc3978264c5a4da8ae6fe66f003c1a8eae81
Antoine Choppin <antoine@japonophile.com>
2020-11-08 20:37:22 +0900

    Day 4 part 2

--- src/aoc2019/d4.clj
+++ src/aoc2019/d4.clj
@@ -27,10 +27,37 @@
     (is (= false (meets-criteria? 223450)))
     (is (= false (meets-criteria? 123789)))))
 
+; part 2
+
+(with-test
+  (defn meets-criteria-2?
+    [n]
+    (let [d (str n)]
+      (and
+        (or (and (= (get d 0) (get d 1)) (not= (get d 1) (get d 2)))
+            (and (not= (get d 0) (get d 1)) (= (get d 1) (get d 2)) (not= (get d 2) (get d 3)))
+            (and (not= (get d 1) (get d 2)) (= (get d 2) (get d 3)) (not= (get d 3) (get d 4)))
+            (and (not= (get d 2) (get d 3)) (= (get d 3) (get d 4)) (not= (get d 4) (get d 5)))
+            (and (not= (get d 3) (get d 4)) (= (get d 4) (get d 5))))
+        (<= (int (get d 0))
+            (int (get d 1))
+            (int (get d 2))
+            (int (get d 3))
+            (int (get d 4))
+            (int (get d 5))))))
+  (testing "meets password criteria (2)"
+    (is (= false (meets-criteria-2? 111111)))
+    (is (= false (meets-criteria-2? 223450)))
+    (is (= false (meets-criteria-2? 123789)))
+    (is (= true (meets-criteria-2? 112233)))
+    (is (= false (meets-criteria-2? 123444)))
+    (is (= true (meets-criteria-2? 111122)))
+    ))
+
 (defn count-passwords
   [rng]
   (let [[st en] (s/split rng #"-")
         start (Integer/parseInt st)
         end   (Integer/parseInt en)]
     (println start end)
-    (count (filter true? (map meets-criteria? (range start (inc end)))))))
+    (count (filter true? (map meets-criteria-2? (range start (inc end)))))))
